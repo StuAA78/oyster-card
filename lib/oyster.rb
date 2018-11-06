f6a3be650aeaaea0abe99307701acdede3daa53f
Stuart Adair <stu@silverka.co.uk>
2018-11-06 15:45:32 +0000

    Cannot top up over maximum card value

--- lib/oyster.rb
+++ lib/oyster.rb
@@ -1,14 +1,17 @@
 class Oystercard
-    attr_reader :balance
+  attr_reader :balance
 
-    DEFAULT_BALANCE = 0
+  DEFAULT_BALANCE = 0
+  MAXIMUM_BALANCE = 90
+  MAX_ERROR = "Cannot top up over maximum balance of £#{MAXIMUM_BALANCE}"
 
-    def initialize(balance = DEFAULT_BALANCE)
-        @balance = balance
-    end
+  def initialize(balance = DEFAULT_BALANCE)
+    @balance = balance
+  end
 
-    def top_up(amount)
-      @balance += amount
-    end
+  def top_up(amount)
+    raise MAX_ERROR if @balance + amount > MAXIMUM_BALANCE
 
+    @balance += amount
+  end
 end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -1,14 +1,16 @@
 require 'oyster'
 
 describe Oystercard do
+  context '#top_up' do
+    it 'should be able to be topped up with an amount' do
+      subject.top_up(5)
+      expect(subject.balance).to eq 5
+    end
 
-  it "should have a balance of zero when created" do
-    expect(subject.balance).to eq 0
+    it 'should not be possible top up over the maximum balance' do
+      max = Oystercard::MAXIMUM_BALANCE
+      error = Oystercard::MAX_ERROR
+      expect { subject.top_up(max + 5) }.to raise_error(RuntimeError, error)
+    end
   end
-
-  it "should be able to be topped up with an amount" do
-    subject.top_up(5)
-    expect(subject.balance).to eq 5
-  end
-
 end
